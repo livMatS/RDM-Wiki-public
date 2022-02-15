7600c9e63b2093efc212337d62fdea0bcedff54e
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-02-15 17:46:32 +0100

    ENH: Patch template to remove figure captions in PDF

--- rdm/dtool/template/get_template.sh
+++ rdm/dtool/template/get_template.sh
@@ -5,3 +5,7 @@ NAME="Eisvogel-${VERSION}"
 TAR_ARCHIVE="${NAME}.tar.gz"
 wget "https://github.com/Wandmalfarbe/pandoc-latex-template/releases/download/v${VERSION}/${TAR_ARCHIVE}"
 tar -xf "${TAR_ARCHIVE}"
+
+# apply patch to disable figure captions originally created with
+#   diff -u eisvogel.latex.bkp eisvogel.latex > eisvogel.patch
+patch eisvogel.latex eisvogel.patch
